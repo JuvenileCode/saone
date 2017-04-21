c92087463e3c3bc8385007643790c18d04bcd32f
bin_l <dota.liu.bin@gmail.com>
2017-04-21 20:56:44 +0800

    spring-boot one commit!

---
+++ src/main/java/com/bademo/SaoneApplication.java
@@ -0,0 +1,13 @@
+package com.bademo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class SaoneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SaoneApplication.class, args);
+    }
+}
