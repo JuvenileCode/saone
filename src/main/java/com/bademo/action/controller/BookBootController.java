e0d0eefcc8677d9b1a32678cc51cbfa3e51f959f
Bin.L <dota.liu.bin@gmail.com>
2017-08-28 16:32:05 +0800

    add swagger config!

--- src/main/java/com/bademo/action/controller/BookBootController.java
+++ src/main/java/com/bademo/action/controller/BookBootController.java
@@ -2,6 +2,8 @@ package com.bademo.action.controller;
 
 import com.bademo.action.bean.BookBoot;
 import com.bademo.action.service.BookBootRepository;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -20,11 +22,13 @@ import java.util.List;
  */
 @Controller
 @RequestMapping("/book")
+@Api(value = "API-VehiclesController", description = "Book接口swagger测试")
 public class BookBootController {
 
     @Autowired
     private BookBootRepository bookBootJpaService;
 
+    @ApiOperation(value = "查询Book接口", notes = "此接口描述xxxxxxxxxxxxx<br/>xxxxxxx<br>值得庆幸的是这儿支持html标签<hr/>", httpMethod = "GET", response = String.class)
     @RequestMapping(value = "/{reader}", method = RequestMethod.GET)
     public String readersBook(@PathVariable String reader, Model model) {
         List<BookBoot> bbt = bookBootJpaService.findByReader(reader);
