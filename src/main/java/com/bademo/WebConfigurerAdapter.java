d07eaafced1e2d70e07171d372ee86ee9083559f
Bin.L <dota.liu.bin@gmail.com>
2017-11-16 13:50:08 +0800

    logback config & access static resources

---
+++ src/main/java/com/bademo/WebConfigurerAdapter.java
@@ -0,0 +1,41 @@
+package com.bademo;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Created Info:访问静态资源
+ * User: dota.liu.bin@gmail.com
+ * Date: 2017/11/16 0016.11:44
+ */
+@Configuration
+public class WebConfigurerAdapter extends WebMvcConfigurerAdapter {
+
+    /**
+     * 可以覆盖默认的eroor页面
+     * @param configurer
+     */
+   /* @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/error").setViewName("error.html");
+        registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
+    }*/
+
+    @Override
+    public void configurePathMatch(PathMatchConfigurer configurer) {
+        super.configurePathMatch(configurer);
+        configurer.setUseSuffixPatternMatch(false);
+    }
+
+    /**
+     * 配置静态访问资源
+     * @param registry
+     */
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**").addResourceLocations("classpath:/");
+        super.addResourceHandlers(registry);
+    }
+}
