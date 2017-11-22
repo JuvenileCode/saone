ae4b5f9dd6b125f5c1d509297ab4f8e53123693c
Bin.L <dota.liu.bin@gmail.com>
2017-11-22 11:39:26 +0800

    访问静态资源添加对swaggerUI的访问

--- src/main/java/com/bademo/WebConfigurerAdapter.java
+++ src/main/java/com/bademo/WebConfigurerAdapter.java
@@ -35,7 +35,12 @@ public class WebConfigurerAdapter extends WebMvcConfigurerAdapter {
      */
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/**").addResourceLocations("classpath:/");
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/");
         super.addResourceHandlers(registry);
     }
 }
